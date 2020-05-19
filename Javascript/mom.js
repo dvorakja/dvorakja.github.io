8f9d7c4c4fa41c815cdc6b97105ccb2e93a03c9a
James Jr <JamesJr.@Jamess-MacBook-Air.local>
2020-05-19 13:43:57 -0500

    Final

---
+++ Javascript/mom.js
@@ -0,0 +1,14 @@
+alert("Page is fully loaded");
+if (confirm("Page is Secure")) {
+  txt = "You pressed OK!";
+} else {
+  txt = "You pressed Cancel!";
+}
+
+$(document).ready(function(){
+  /*
+  alert("I am fully loaded");
+  console.log("Hello Lisa Dvorak");
+  */
+var d = new Date(2018, 11);
+});
